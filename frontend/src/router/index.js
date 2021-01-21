61ebccd5d17b9bba3b86391906aafe273621c928
Oscar Catota <ocatota@gmail.com>
2021-01-21 02:36:09 -0500

    Bulma

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -13,10 +13,27 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/client',
+    name: 'Clients',
+    component: () => import('../views/Clients/Clients.vue')
+  },
+  {
+    path: '/contracts',
+    name: 'Contracts',
+    component: () => import('../views/Contracts/Contracts.vue')
+  },
+  {
+    path: '/reports',
+    name: 'Reports',
+    component: () => import('../views/Contracts/Reports.vue')
   }
 ]
 
